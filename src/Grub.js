cb9729bc03692565043f70a891cf630f8ec4dd19
Tina Ko <tinarchan@gmail.com>
2017-02-13 11:11:24 -0800

    Add Grub class in pseudoclassical style

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,14 @@
 var Grub = function() {
+  // this = Object.create(Object.prototype)
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+  // return this;
 };
+
+Grub.prototype.eat = function () {
+
+};
+
+// var grub = new Grub();
 
